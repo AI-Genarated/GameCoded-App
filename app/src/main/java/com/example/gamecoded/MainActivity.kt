34cd1ebf164ce06613c1b26f1248ea1e025a341c
mkndebejhb024 <mkndebejhb024@student.wethinkcode.co.za>
2025-09-26 23:59:26 +0200

    add lessons page

--- app/src/main/java/com/example/gamecoded/MainActivity.kt
+++ app/src/main/java/com/example/gamecoded/MainActivity.kt
@@ -11,37 +11,4 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.gamecoded.ui.theme.GameCodedTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GameCodedTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GameCodedTheme {
-        Greeting("Android")
-    }
-}
